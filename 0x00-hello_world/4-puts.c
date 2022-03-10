0f465bc6918a89385eac128b2fc8978508727297
Tobe <tobennannabeze@gmail.com>
2022-03-10 10:50:17 -0800

    wrote my first c program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*
+ * main: prints the phrase, 'programming is like building a multilingual puzzle
+ * Return: 0 if exit successful; non-zero otherwise
+ */
+
+int main()
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return 0;
+} 
