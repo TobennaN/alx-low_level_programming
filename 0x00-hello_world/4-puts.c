727c553e0aeaf10c8d0e5afea06336af545be676
Tobe <tobennannabeze@gmail.com>
2022-03-10 12:54:31 -0800

    tired

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 
 /**
- * main: displays the message- ""Programming is like building a multilingual puzzle"
- * return: 0 for successful exit
+ * main: prints the message ""Programming is like building a multilingual puzzle
+ * return: 0 if exited successfully; else non-zero
  */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
